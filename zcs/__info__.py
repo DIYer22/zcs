d533d9e8224d4b9bb8fef8652fbddf99a020b84a
Lei <ylxx@live.com>
2020-09-24 00:54:00 +0800

    better setup

---
+++ zcs/__info__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.16"
+__description__ = "Z Configuration System: a flexible powerful configuration system which takes advantage of both argparse and yacs"
+__license__ = "MIT"
+__author__ = "DIYer22"
+__author_email__ = "ylxx@live.com"
+__maintainer__ = "DIYer22"
+__maintainer_email__ = "ylxx@live.com"
+__github_username__ = "DIYer22"
+__url__ = "https://github.com/DIYer22/zcs"
+__support__ = "https://github.com/DIYer22/zcs/issues"
+__classifiers__ = [
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+]
--- zcs/__init__.py
+++ zcs/__init__.py
@@ -6,17 +6,7 @@ from __future__ import unicode_literals
 Z Configuration System: a flexible powerful configuration system 
 which takes advantage of both argparse and yacs
 """
-__version__ = "0.1.15"
-__short_description__ = "Z Configuration System: a flexible powerful configuration system which takes advantage of both argparse and yacs"
-__license__ = "MIT"
-__author__ = "DIYer22"
-__author_email__ = "ylxx@live.com"
-__maintainer__ = "DIYer22"
-__maintainer_email__ = "ylxx@live.com"
-__github_username__ = "DIYer22"
-__github_url__ = "https://github.com/DIYer22/zcs"
-__support__ = "https://github.com/DIYer22/zcs/issues"
-
+from .__info__ import __version__
 
 from .config import CfgNode, argument, identity
 from .config import parser, parse_args, merge_by_args
